976bdc26086ef032a9cbf56b53e64ce896f1d501
mschop <mschopdev@gmail.com>
2021-08-23 22:27:25 +0200

    Add funny start page

--- template/index.php
+++ template/index.php
@@ -4,7 +4,26 @@ return _document(
     _html(
         _head(),
         _body(
-            'Hello World from Phespro',
+            _div('Hello World from Phespro'),
+            _div('The best PHP framework'),
+            _div('On the hole world'),
+            _div(';-)'),
+            _div('Happy Coding'),
+            _div('And have fun'),
+
+            _style(_raw("
+                body { background-color: #242424; }
+                div {
+                    margin-top: 5rem;
+                    color: #fff;
+                    text-align: center;
+                    transform: rotate(-10deg);
+                }
+
+                div:nth-child(even) {
+                    transform: rotate(10deg);
+                }
+            ")),
         ),
     ),
 );
